d2406156e2ea6d8fee6c767133cffd112f669a61
NikaChkoidze26 <nickchkoidze@gmail.com>
2024-11-03 18:09:15 +0400

    commit

---
+++ level 51/homework/level51.py
@@ -0,0 +1,5 @@
+def summation(num):
+    return sum(range(1, num + 1))
+
+print(summation(2)) 
+print(summation(8))  
---
+++ level 51/homework/level51V2.py
@@ -0,0 +1,31 @@
+def greet(language):
+    
+    greetings = {
+        "english": "Welcome",
+        "czech": "Vitejte",
+        "danish": "Velkomst",
+        "dutch": "Welkom",
+        "estonian": "Tere tulemast",
+        "finnish": "Tervetuloa",
+        "flemish": "Welgekomen",
+        "french": "Bienvenue",
+        "german": "Willkommen",
+        "irish": "Failte",
+        "italian": "Benvenuto",
+        "latvian": "Gaidits",
+        "lithuanian": "Laukiamas",
+        "polish": "Witamy",
+        "spanish": "Bienvenido",
+        "swedish": "Valkommen",
+        "welsh": "Croeso",
+        "georgian": "Gamarjoba"
+    }
+    
+    
+    return greetings["english"]
+
+
+print(greet("spanish"))  
+print(greet("georgian"))
+print(greet("french"))   
+print(greet("japanese")) 
