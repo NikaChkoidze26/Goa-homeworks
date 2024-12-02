d3ea732772f815ca59b1c694b991e862b8bae0c3
NikaChkoidze26 <nickchkoidze@gmail.com>
2024-12-02 19:55:33 +0400

    commit

---
+++ level 56/homework/level56.py
@@ -0,0 +1,2 @@
+def reverse_seq():
+    return list(range(0, -1))
---
+++ level 56/homework/level56V2.py
@@ -0,0 +1,2 @@
+def array_plus_array(arr1,arr2):  
+    return sum(arr1) + sum(arr2) #sum ფუნქცია გვეხმარაება შევაჯამოთ arr1 და arr2-ის კომპონენტები.
