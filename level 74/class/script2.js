6b841e400361d4eeb862602f7672144c99c7874a
NikaChkoidze26 <nickchkoidze@gmail.com>
2025-01-22 20:49:55 +0400

    commit

---
+++ level 74/class/script2.js
@@ -0,0 +1,13 @@
+switch (coffee){
+    case 1:
+        console.log("Americano");
+        break;
+    case 2:
+        console.log("Espresso");
+        break;
+    case 3:
+        console.log("Cappuccino");
+        break;
+    default:
+        console.log("Unknown");
+} 
