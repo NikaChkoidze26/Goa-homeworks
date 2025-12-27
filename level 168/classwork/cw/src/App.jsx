c351a3b70f4ef45b6e3612f3c9c8a8a9008b0457
NikaChkoidze26 <nickchkoidze@gmail.com>
2025-12-27 23:48:39 +0400

    commit

---
+++ level 168/classwork/cw/src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="bg-gray-200 p-6">
+
+      <h1 className="font-light text-red-600 bg-white">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Facere, repellendus! Exercitationem excepturi voluptatum nihil tempore tempora quidem molestias adipisci ea facilis nemo cupiditate sapiente quas quasi unde soluta, mollitia commodi.
+      </h1>
+
+      <p className="font-medium text-blue-600 bg-yellow-100">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vitae odit explicabo ex dolorem voluptatibus enim odio facere aut cupiditate possimus, deleniti dolor nostrum commodi maxime inventore saepe vero ad qui!
+      </p>
+
+      <div className="font-black text-white bg-green-600">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus iure, adipisci veritatis voluptatum et commodi rem cum sed dolor libero facere eius numquam. Cum debitis tenetur sequi nesciunt, nihil non?
+      </div>
+
+    </div>
+  )
+}
+
+export default App
