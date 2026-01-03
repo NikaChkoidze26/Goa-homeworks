fcd4222b454e33d17e663c96add0fa3e027d9dfb
NikaChkoidze26 <nickchkoidze@gmail.com>
2026-01-03 18:50:59 +0400

    commit

---
+++ level 169/classwork/cw/src/App.jsx
@@ -0,0 +1,23 @@
+function App() {
+  return (
+    <div className="flex items-center justify-center bg-gray-200">
+      
+      <div className="flex gap-2">
+        <div className="bg-red-400 text-white">
+          hello world
+        </div>
+
+        <div className="bg-blue-400 text-white">
+          hello world
+        </div>
+
+        <div className="bg-green-400 text-white">
+          hello world
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default App
