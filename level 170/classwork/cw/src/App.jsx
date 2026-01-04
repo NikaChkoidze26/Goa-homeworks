1d66a40cdeefe1758dbc277662310ea179ea3892
NikaChkoidze26 <nickchkoidze@gmail.com>
2026-01-04 22:50:52 +0400

    commit

---
+++ level 170/classwork/cw/src/App.jsx
@@ -0,0 +1,38 @@
+function App() {
+  return (
+    <div className="bg-gray-100">
+
+      <div className="bg-white p-4">
+
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQezi6j-e_XsHOGC2Gqiv1M3S4tn7amKZBEQA&s"
+          alt="img"
+          className="w-full md:w-1/2"
+        />
+
+        <div className="mt-4 md:mt-0 md:ml-2">
+          <h1 className="text-xl md:text-2xl font-bold">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iusto aperiam doloribus blanditiis similique explicabo dolores. Perspiciatis, iusto pariatur quis labore placeat magni sequi fugiat! Aliquam, velit eligendi? Aut, tempore repellendus.
+          </h1>
+
+
+          <h2 className="text-gray-600 mt-1 md:mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum unde velit atque illum reiciendis consequatur, maxime ipsa, quod tenetur autem officiis, ratione necessitatibus quam enim quaerat quas? Vitae, minus ex.
+          </h2>
+
+          <p className="mt-2 ml-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat, facilis voluptatibus esse voluptatem, error ratione facere doloribus quos at nulla eius quas fugiat culpa, voluptatum voluptas ipsa delectus sit minus!
+          </p>
+
+          <button className="mt-4 bg-blue-500 text-white">
+            Click Me
+          </button>
+        </div>
+
+      </div>
+
+    </div>
+  )
+}
+
+export default App
