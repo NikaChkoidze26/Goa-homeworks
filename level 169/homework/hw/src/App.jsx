07b968ab92706eeb561d6d60ba413abad2f3c657
NikaChkoidze26 <nickchkoidze@gmail.com>
2026-01-10 20:21:45 +0400

    commit

---
+++ level 169/homework/hw/src/App.jsx
@@ -0,0 +1,7 @@
+<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+  <div class="bg-green-300 p-4">1</div>
+  <div class="bg-green-400 p-4">2</div>
+  <div class="bg-green-500 p-4">3</div>
+  <div class="bg-green-600 p-4">4</div>
+</div>
+
---
+++ level 169/homework/hw/src/main.jsx
@@ -0,0 +1,4 @@
+<div class="flex flex-col md:flex-row gap-4">
+  <div class="bg-red-300 p-4">Box 1</div>
+  <div class="bg-red-400 p-4">Box 2</div>
+</div>
