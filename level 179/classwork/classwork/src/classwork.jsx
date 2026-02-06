07d6abc4550587cd87de4aa5e17f9c492960ac6e
NikaChkoidze26 <nickchkoidze@gmail.com>
2026-02-06 23:23:12 +0400

    commit

---
+++ level 179/classwork/classwork/src/classwork.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+
+      <button onClick={increaseCount}>
+        Add +1
+      </button>
+    </div>
+  );
+}
+
+export default App;
